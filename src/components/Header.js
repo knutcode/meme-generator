f0999d1cb26726c98f19ed5f4c89f107bb282146
knutc <knutc.kodehode@gmail.com>
2022-08-17 14:11:14 +0200

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header className="flex">
+      <h1>Meme Generator</h1>
+      <p>React Course - Project 3</p>
+    </header>
+  );
+};
+
+export default Header;
