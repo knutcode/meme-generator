eed54d17ee7d632f8cd5b1ad6bbcf87e88a6ce84
knutc <knutc.kodehode@gmail.com>
2022-08-18 13:52:30 +0200

    renders img from data.js with getImage() function

--- src/components/Memes.js
+++ src/components/Memes.js
@@ -1,28 +1,21 @@
-import { useRef, useEffect } from "react";
+import { useState } from "react";
 import memesData from "../data/data.js";
 
 const Memes = () => {
-  const ref = useRef(null);
+  const [memeImage, setNewImage] = useState("");
 
-  useEffect(() => {
-    const handleClick = () => {
-      console.log("Button clicked");
-    };
-
-    const element = ref.current;
-
-    element.addEventListener("click", handleClick);
-
-    return () => {
-      element.removeEventListener("click", handleClick);
-    };
-  }, []);
+  function getImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    setNewImage(memesArray[randomNumber].url);
+  }
 
   return (
     <div className="container flex">
       <input type="text" placeholder="Top text"></input>
       <input type="text" placeholder="Bottom text"></input>
-      <button ref={ref}>Get a new meme image</button>
+      <button onClick={getImage}>Get a new meme image</button>
+      <img src={memeImage} alt="" />
     </div>
   );
 };
